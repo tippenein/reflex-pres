50a4cc143ae906838a367e7df5e88f8e5bddc54b
brady.ouren <brady.ouren@gmail.com>
2016-11-01 22:17:02 -0700

    initial pomodoro example

---
+++ src/Main.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE RecursiveDo #-}
+
+module Main where
+
+import           Reflex
+import           Reflex.Dom
+
+import           Data.Decimal
+import           Data.Time.Clock (getCurrentTime, UTCTime)
+
+import qualified Widget
+
+data TimerEvent
+  = Start Decimal -- starting countdown
+  | Break Decimal
+  | TimerTick Decimal
+
+timer :: MonadWidget t m => UTCTime -> m ()
+timer t0 = el "div" $ do
+  let increment = 1.0 -- this one's a decimal
+  tick <- tickLossy 1.0 t0 -- this one's a nominal diff time
+
+  start <- button "Start"
+  take_a_break <- button "Take a Break"
+
+  countdownFrom <- Widget.readableInput $ def
+    & attributes .~ constDyn (mconcat [ "placeholder" =: "pomodoro length..."
+                                      , "class" =: "limit"
+                                      ])
+  countdownDyn <- holdDyn 25.0 countdownFrom
+
+  breakAmount <- Widget.readableInput $ def 
+    & attributes .~ constDyn (mconcat [ "placeholder" =: "chill for a sec."
+                                      ])
+  breakDyn <- holdDyn 5.0 breakAmount
+
+  rec let events = leftmost
+            [ (\(curr, _) -> TimerTick curr) <$> attachDyn (constDyn 0.0) tick
+            , (\(from, _) -> Start from) <$> attachDyn countdownDyn start
+            , (\(from, _) -> Break from) <$> attachDyn breakDyn take_a_break
+            ]
+
+      remaining <- foldDyn (\ev curr -> case ev of
+              TimerTick _whatever ->
+                if curr - increment >= 0
+                then curr - increment
+                else curr
+              Start limit' -> limit'
+              Break b -> b
+          ) (0.0 :: Decimal) events
+
+      elapsedText <- mapDyn show remaining
+      _ <- widgetHold elapsedWidget $ dynText elapsedText <$ start
+  pure ()
+
+elapsedWidget :: MonadWidget t m => m ()
+elapsedWidget = el "p" $ text ""
+
+main :: IO ()
+main = do
+  tStart <- getCurrentTime
+  mainWidgetWithHead (Widget.headElement "timer") $
+    elClass "div" "container" $ do
+      timer tStart
+      pure ()
---
+++ src/Widget.hs
@@ -0,0 +1,88 @@
+{-# LANGUAGE RecursiveDo #-}
+
+module Widget where
+
+import           Reflex
+import           Reflex.Dom
+
+import           Data.Map         (Map)
+import qualified Data.Map         as Map
+import           Data.Maybe
+import           Data.Monoid
+import           Data.Time.Clock  (UTCTime)
+import           Data.Time.Format (defaultTimeLocale, parseTimeM)
+
+import           Text.Read        (readMaybe)
+
+type PageTitle = String
+
+headElement :: MonadWidget t m => PageTitle -> m ()
+headElement title = do
+  el "title" (text title)
+  styleSheet "css/style.css"
+  styleSheet "https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
+  styleSheet "http://fonts.googleapis.com/css?family=Lato"
+  styleSheet "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css"
+  where
+    styleSheet _link = elAttr "link" (Map.fromList [
+          ("rel", "stylesheet")
+        , ("type", "text/css")
+        , ("href", _link)
+      ]) $ pure ()
+
+readableInput :: (MonadWidget t m, Read a) => TextInputConfig t -> m (Event t a)
+readableInput conf = do
+    c <- textInput conf
+    pure $ fmapMaybe readMaybe $ _textInput_input c
+
+maybeButton :: MonadWidget t m
+            => Dynamic t Bool -- ^ Is the button enabled?
+            -> String -- ^ Static button label
+            -> m (Event t ())
+maybeButton enabled label = do
+    attrs <- forDyn enabled $ \e -> monoidGuard (not e) $ "disabled" =: "disabled"
+    (btn, _) <- elDynAttr' "button" attrs $ text label
+    pure $ domEvent Click btn
+
+datePicker :: MonadWidget t m
+           => Dynamic t Bool -- ^ Widget enabled?
+           -> m (Dynamic t (Maybe UTCTime))
+datePicker enabled = do
+    rec raw <- textInput $ def & textInputConfig_attributes .~ attrs
+        date <- mapDyn (parseTimeM True defaultTimeLocale "%F") $ _textInput_value raw
+        attrs <- dynCombine date enabled $ \d e ->
+            monoidGuard (isNothing d) ("style" =: "color: red") <>
+            monoidGuard (not e) ("disabled" =: "disabled")
+    return date
+
+selectableList :: (MonadWidget t m, Ord k)
+               => Dynamic t (Maybe k)
+               -- ^ Key of element that may be selected
+               -> Dynamic t (Map k v)
+               -- ^ Map of elements to be shown in the list
+               -> (Dynamic t Bool -> Dynamic t v -> m (Event t a))
+               -- ^ Action that renders a widget for an element. The element may fire events
+               -> m (Event t k)
+               -- ^ List fires events whenever an element is selected
+selectableList selection elems mkEntry = do
+    selectEntry <- listWithKey elems $ \k v -> do
+        isSelected <- forDyn selection $ \s -> s == Just k
+        fmap (const k) <$> mkEntry isSelected v
+    switchPromptlyDyn <$> mapDyn (leftmost . Map.elems) selectEntry
+
+monoidGuard :: Monoid a => Bool -> a -> a
+monoidGuard p a = if p then a else mempty
+
+dynCombine :: (Reflex t, MonadHold t m)
+           => Dynamic t a -> Dynamic t b
+           -> (a -> b -> c)
+           -> m (Dynamic t c)
+dynCombine a b f = combineDyn f a b
+
+dynCombine3 :: (Reflex t, MonadHold t m)
+            => Dynamic t a -> Dynamic t b -> Dynamic t c
+            -> (a -> b -> c -> d)
+            -> m (Dynamic t d)
+dynCombine3 da db dc f = do
+    dg <- combineDyn f da db
+    combineDyn (\g c -> g c) dg dc
